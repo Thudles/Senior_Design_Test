4a09b693ed879b0050944f974e1e4d51714fce34
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-15 23:57:27 -0500

    Fixed Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Home } from "./Pages/Home/Home";
 import { Home_logedout } from "./Pages/Home/Home_logedout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Account } from "./Pages/Account";
 import { Menu } from "./Pages/Menu";
 import { Reviews } from "./Pages/Reviews";
@@ -10,15 +10,13 @@ import { Request } from "./Pages/Request";
 const App = () => {
   return (
     <div className="p-3">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/review" element={<Reviews />} />
-          <Route path="/request" element={<Request />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/review" element={<Reviews />} />
+        <Route path="/request" element={<Request />} />
+      </Routes>
     </div>
   );
 };
