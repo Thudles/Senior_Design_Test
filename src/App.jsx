a3e7661378e24ee4891ce375919f11939a333c49
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-15 22:57:52 -0500

    Routes Made in App.jsx, Pages made for those routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Home } from "./Pages/Home/Home";
 import { Home_logedout } from "./Pages/Home/Home_logedout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Account } from "./Pages/Account";
+import { Menu } from "./Pages/Menu";
+import { Reviews } from "./Pages/Reviews";
+import { Request } from "./Pages/Request";
 
 const App = () => {
   return (
-    <div className="p-5 ">
-      <Home />
-      {/* <Home_logedout /> */}
+    <div className="p-3">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/review" element={<Reviews />} />
+          <Route path="/request" element={<Request />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
