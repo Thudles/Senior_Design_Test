8206e6a1508414ed5d8a1e9715b438afd86753d4
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-21 21:29:41 -0500

    Request page and commonent work

--- Frontend/src/Pages/Request.jsx
+++ Frontend/src/Pages/Request.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
+import request from "../Data/request";
 
 export const Request = () => {
   return (
     <div className="">
       <Navbar />
       <div className="rounded-lg h-[calc(100vh-64px)] overflow-y-auto w-[100%] bg-black p-3 text-white mt-[51px]">
-        Request
+        <div className="flex gap-1">
+          {request.map((request) => (
+            <div>{request.priority}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- Frontend/src/components/Home/Request.jsx
+++ Frontend/src/components/Home/Request.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+
+import request from "../../Data/request";
 
 export const Request = ({ colspan, rowspan }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { dragFree: true, loop: true },
+    [Autoplay()]
+  );
+
   return (
-    <div
-      className={` ${colspan} ${rowspan}  rounded-lg  bg-projblack p-3 text-white h-[100%] `}
-    >
-      Request
+    <div className={`rounded-lg bg-black p-3 text-black h-[100%] `}>
+      <div className="overflow-hidden h-[100%]" ref={emblaRef}>
+        <div className="flex touch-pan-y touch-pinch-zoom h-[100%]">
+          {request.map((request) => (
+            <div className="flex-[0_0_15%] rounded-md min-w-0 bg-white h-[100%] ml-2 p-1">
+              {request.priority}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
