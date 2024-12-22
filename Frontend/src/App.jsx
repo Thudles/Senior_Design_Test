2d893ef3ca3a7de1b47a0d42adc0435edd590c05
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-21 21:30:26 -0500

    tab title feature

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const originalTitle = "Husky Dining";
+  const inactiveTitle = "...still hungry :(";
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      document.title = document.hidden ? inactiveTitle : originalTitle;
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
   return (
     <div className="p-1.5">
       <ToastContainer />
