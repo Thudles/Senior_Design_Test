7d314c557bfe481bdae62abe238446656c8d5db4
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-28 10:52:21 -0600

    menu component partially implemented

--- Frontend/src/components/Home/Menu.jsx
+++ Frontend/src/components/Home/Menu.jsx
@@ -1,12 +1,87 @@
-import React from "react";
-import { Request } from "./Request";
+import React, { useState, useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoscroll from "embla-carousel-auto-scroll";
+import { useGetRequestsQuery } from "../../slices/requestApiSlice";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import axios from "axios";
 
-export const Menu = ({ colspan, rowspan, color }) => {
+export const Menu = ({ color }) => {
+  const { data: requests = [], isLoading, isError } = useGetRequestsQuery();
+
   return (
-    <div
-      className={`${colspan} ${rowspan} ${color} rounded-lg  p-3 h-[100%] text-white`}
-    >
-      Menu
+    <div className={`${color} rounded-lg  p-3 h-[100%] text-white`}>
+      <PanelGroup direction="vertical" className="rounded-md">
+        <Panel defaultSize={85}>
+          <div className="h-[100%]"></div>
+        </Panel>
+
+        <PanelResizeHandle className="h-2" />
+
+        <Panel defaultSize={15}>
+          <Carousel>
+            {isLoading ? (
+              Array(10)
+                .fill(null)
+                .map((_, index) => <Card key={index} isloading={isLoading} />)
+            ) : isError ? (
+              <div>{isError?.data?.message || error.error}</div>
+            ) : (
+              requests.map((data) => (
+                <Card data={data} key={data._id} isloading={isLoading} />
+              ))
+            )}
+          </Carousel>
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
+};
+
+const Card = ({ data, isloading }) => {
+  if (isloading) {
+    return (
+      <div className="flex-[0_0_20%] rounded-lg min-w-0 bg-gray-200 text-black h-[100%] ml-2 p-1 animate-pulse">
+        <div className="h-4 rounded-sm bg-gray-100 animate-pulse"></div>
+        <div className="space-y-1.5 pt-2.5">
+          <div className="h-3 bg-gray-100 animate-pulse rounded-sm"></div>
+          <div className="h-3 bg-gray-100 animate-pulse rounded-sm w-5/6"></div>
+          <div className="h-3 bg-gray-100 animate-pulse rounded-sm w-3/6"></div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-[0_0_20%] rounded-lg min-w-0 bg-white text-black h-[100%] ml-1 p-0.5 border-[2px] border-black">
+      <PanelGroup direction="horizontal">
+        <Panel defaultSize={30} className="bg-white rounded-l-md">
+          {data.priority}
+        </Panel>
+        <PanelResizeHandle disabled="true" className="bg-white w-0.5" />
+        <Panel defaultSize={70} className="bg-white rounded-r-lg"></Panel>
+      </PanelGroup>
+    </div>
+  );
+};
+
+const Carousel = ({ children }) => {
+  const autoscrollOptions = {
+    speed: 1,
+    startDelay: 300,
+    stopOnInteraction: false,
+    stopOnMouseEnter: true,
+  };
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { dragFree: true, loop: true },
+    [Autoscroll(autoscrollOptions)]
+  );
+
+  return (
+    <div className="overflow-hidden h-[100%]" ref={emblaRef}>
+      <div className="flex touch-pan-y touch-pinch-zoom h-[100%]">
+        {children}
+      </div>
     </div>
   );
 };
