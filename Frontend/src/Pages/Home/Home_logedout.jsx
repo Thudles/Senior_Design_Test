5a94b9659ded1dc3d1fa680729e4fc4f27c35938
Chikwendu Mgbechi <chikwe2002@gmail.com>
2024-12-28 10:55:31 -0600

    updated layout

--- Frontend/src/Pages/Home/Home_logedout.jsx
+++ Frontend/src/Pages/Home/Home_logedout.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Menu } from "../../components/Home/Menu";
 import { Transactions } from "../../components/Home/Transactions";
 import { Navbar } from "../../components/Navbar/Navbar";
-import { Request } from "../../components/Home/Request";
-import { MenuLoggedout } from "../../components/Home/MenuLoggedout";
+import { Review } from "../../components/Home/Reviews";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 export const Home_logedout = () => {
@@ -12,22 +11,22 @@ export const Home_logedout = () => {
       <Navbar title={"home"} />
       <div className=" rounded-lg h-[calc(100vh-64px)] overflow-y-auto w-[100%] mt-[51px]">
         <PanelGroup direction="vertical">
-          <Panel defaultSize={70}>
+          <Panel defaultSize={80}>
             <Menu color={"bg-primary"} />
           </Panel>
 
           <PanelResizeHandle disabled={true} className="h-0.5" />
 
-          <Panel defaultSize={30}>
+          <Panel defaultSize={20}>
             <PanelGroup direction="horizontal">
               <Panel defaultSize={70}>
-                <Request colspan={"col-span-12"} rowspan={"row-span-3"} />
+                <Review bgcolor={"bg-black"} />
               </Panel>
 
               <PanelResizeHandle disabled={true} className="w-0.5" />
 
               <Panel defaultSize={30}>
-                <div className="rounded-lg bg-white p-3 text-white h-[100%]"></div>
+                <div className="rounded-lg bg-[#fff] p-3 text-white h-[100%]"></div>
               </Panel>
             </PanelGroup>
           </Panel>
